b56223947fcd7dea8a105c4c9106d549bb2ae4b0
paul-katkov <79351222+paul-katkov@users.noreply.github.com>
2021-02-22 18:46:16 -0500

    Update ExpediaProject.py

--- ExpediaProject/ExpediaProject.py
+++ ExpediaProject/ExpediaProject.py
@@ -157,6 +157,8 @@ def main():
     else:
         ExtractDataIntoLog(month, year, summary_sheet, voc_sheet, summary_row_index, voc_col_index)
         logging.info(">SESSION OVER<\n")
+                     
+    excel.close()
 
 if __name__ == '__main__':
     main()
